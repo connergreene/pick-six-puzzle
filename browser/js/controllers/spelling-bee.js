a13d2cb54309f6a2b42b406a63670ca2bbf13a8b
connergreene <csg1922@gmail.com>
2017-08-25 21:21:23 -0400

    modularized puzzle making into services

--- browser/js/controllers/spelling-bee.js
+++ browser/js/controllers/spelling-bee.js
@@ -1,45 +1,6 @@
 'use strict'
-app.controller('spellingBeeCtrl', function ($scope, checkFactory) {
-	function Puzzle(){
-		this.letters = "";
-	 	this.answerKey = [];
-	}
-
-	Puzzle.prototype.generatePuzzle = function(){
-		var consonants = "BCDFGHJKLMNPQRSTVWXYZ";
-		var vowels = "AEIOU";
-
-		//2 to 5 vowels
-		for(var i = 0; i <= Math.random() * (6 - 2)+ 2; i++){
-			var vowel = vowels.charAt(Math.floor(Math.random() * vowels.length));
-			if(!this.letters.includes(vowel)){
-				this.letters += vowel;
-			}
-		}
-
-		//other letters
-		var len = this.letters.length;
-		for (var i = 0; i < 7 - len; i++){
-			var consonant = consonants.charAt(Math.floor(Math.random() * consonants.length));
-			if(!this.letters.includes(consonant)){
-				this.letters += consonant;
-			}
-			else{
-				i--;
-			}
-		}
-
-		return this;
-	}
-
-	Puzzle.prototype.getAnswerKey = function(){
-		return checkFactory.checkSpellingBee(this.letters)
-		.then(function(answers){
-			return answers;
-		});
-	}
-
-
+app.controller('spellingBeeCtrl', function ($scope, checkFactory, spellingBee) {
+	
 	$scope.guess = "";
 	$scope.addValue = function(val){
 		$scope.guess += val;
@@ -87,7 +48,7 @@ app.controller('spellingBeeCtrl', function ($scope, checkFactory) {
 
 	$scope.makeFullPuzz = function(){
 		$scope.clear();
-		$scope.myPuzz = new Puzzle;
+		$scope.myPuzz = new spellingBee;
 		$scope.myPuzz.generatePuzzle();
 		$scope.correctAnswers = [];
 		$scope.showAnswers = false;

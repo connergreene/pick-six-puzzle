507fff68bd0e2099eb0108f903be76b5ce6de0e4
connergreene <csg1922@gmail.com>
2017-09-12 13:47:37 -0400

    password checker

--- browser/js/controllers/signup.js
+++ browser/js/controllers/signup.js
@@ -1,13 +1,20 @@
 'use strict';
 
 app.controller('SignupCtrl', function ($scope, UserFactory, $state) {
+  $scope.checkPassword = '';
+  $scope.passwordError = false;
   $scope.signup = function(){
-    UserFactory.signup($scope.userData)
-    .then(function(){
-      $state.go('home')
-    }, function(){
-      $scope.userData = {}
-    })
+  	if($scope.checkPassword === $scope.userData.password){
+	    UserFactory.signup($scope.userData)
+	    .then(function(){
+	      $state.go('home')
+	    }, function(){
+	      $scope.userData = {}
+	    })
+	}
+	else{
+		$scope.passwordError = true;
+	}
   }
 
 });
